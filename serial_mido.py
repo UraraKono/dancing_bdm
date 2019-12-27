384ea502348c80d91ee53d41601bd592ab2b35e4
Naho Tomiki <t.naho.1737@gmail.com>
2019-12-27 11:58:44 +0900

    ソフトウェアリセットによるバグ修正と無線対応

--- serial_mido.py
+++ serial_mido.py
@@ -10,23 +10,27 @@ import time
 ports = mido.get_output_names()
 port = mido.open_output(ports[0])
 print("port:", port)
-ser = serial.Serial("/dev/cu.usbserial-1410", 9600, timeout=None) 
+print("please input the usbserial number (14110)")
+num = input()
+sername = "/dev/cu.usbserial-" + num
+ser = serial.Serial(sername, 9600, timeout=None) 
 
 print("please input the filename")
 filename = input()
 mid = MidiFile(filename)
 tempo_original = 500000 #us 元のテンポ設定
 bpm_changed = 120
-not_used = ser.readline()
-for i in range(10):
-   not_used = ser.readline()
 
+not_used = ser.readline()
+print(not_used.decode())#MMA8452Q Basic Reading Code!
+not_used = ser.readline()
+print(not_used.decode())#ready
 
 with port as outport:
     for i, msg in enumerate(mid):
         if ser.inWaiting() > 0:
             val_arduino = ser.readline()
-            print("tempo:", val_arduino.decode())
+            print("tempo:", int(val_arduino.decode()) * 2
             bpm_changed = int(val_arduino.decode())
         tempo_changed = mido.bpm2tempo(bpm_changed)
         x = msg.time * tempo_changed / tempo_original
