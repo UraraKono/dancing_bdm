eb246ee14ea4ad31f64c65f7440f435fd2f2a9b6
UraraKono <urara27.k@gmail.com>
2020-01-02 13:15:07 +0900

    情熱大陸を追加

--- serial_mido.py
+++ serial_mido.py
@@ -12,7 +12,7 @@ port = mido.open_output(ports[0])
 print("port:", port)
 print("please input the usbserial number (14110)")
 num = input()
-sername = "/dev/cu.usbserial-" + num
+sername = "/dev/cu.wchusbserial" + num
 ser = serial.Serial(sername, 9600, timeout=None) 
 
 print("please input the filename")
@@ -30,8 +30,8 @@ with port as outport:
     for i, msg in enumerate(mid):
         if ser.inWaiting() > 0:
             val_arduino = ser.readline()
-            print("tempo:", int(val_arduino.decode()) * 2)
-            bpm_changed = int(val_arduino.decode()) * 2
+            print("tempo:", int(val_arduino.decode()) * 1.5)
+            bpm_changed = int(val_arduino.decode()) * 1.5
         tempo_changed = mido.bpm2tempo(bpm_changed)
         x = msg.time * tempo_changed / tempo_original
         time.sleep(x)
